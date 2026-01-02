e05735b42ae446b838c647d63bd15c10a90fd931
NZOPXYT666-2 <ashazpro666@gmail.com>
2026-01-02 23:29:08 +0530

    Initialize 1.18.1

---
+++ src/main/java/org/example/modid/client/ExampleModClient.java
@@ -0,0 +1,11 @@
+package org.example.modid.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        // client init
+    }
+}
